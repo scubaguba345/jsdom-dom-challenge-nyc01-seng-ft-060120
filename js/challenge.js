69246cf6efcb2a29ba8ec807d6b10eb6a51137e8
mayalexandera <maya.blevins@gmail.com>
2020-07-15 15:45:25 -0400

    the old college try

--- js/challenge.js
+++ js/challenge.js
@@ -1,51 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let count = document.getElementById("counter");
+  let timer = 0;
 
-let count = document.getElementById("counter");
-let iter = 0;
+  let btnminus = document.getElementById("minus");
+  let btnplus = document.getElementById("plus");
+  //let input = document.getElementById("input");
+  let btnheart = document.getElementById("heart");
+  let likeUl = document.querySelector(".likes");
+  let likeNum = document.createElement("li");
 
-let btnminus = document.getElementById("minus");
-let btnplus = document.getElementById("plus");
-//let input = document.getElementById("input");
-let btnheart = document.getElementById("heart");
-let likeUl = document.querySelector(".likes");
-let likeNum = document.createElement("li");
+  let btnpause = document.getElementById("pause");
 
-let btnpause = document.getElementById("pause");
-let t = 0;
+  let t = 0;
 
+  let comment = document.getElementById("comment-input");
+  let commentList = document.getElementById("list");
 
-function counter() {
-    //console.log('show at ' + (iter++));
-    count.textContent = (iter++);
-    setTimeout(counter, 1000) 
-    
+  document.addEventListener("submit", function(e) {
+    e.preventDefault();
+    addComment();
+  });
 
-}
+  const addComment = () => {
+    const commentTag = document.createElement("li");
+    commentTag.textContent = comment.value;
+    commentList.appendChild(commentTag);
+  };
 
-btnminus.addEventListener('click', () => {
-    count.textContent = (iter--);
-    //count.value = parseInt(count.value) - 1;
+  function counter() {
+    timer = setTimeout(counter, 1000);
+    count.textContent = timer;
+  }
+
+  btnminus.addEventListener("click", () => {
+    count.textContent = timer--;
+  });
+
+  btnplus.addEventListener("click", () => {
+    count.textContent = timer++;
+  });
+
+  btnheart.addEventListener("click", () => {
+    let val = count.textContent;
+    likeUl.appendChild(likeNum);
+    likeNum.textContent = val;
+  });
+
+  btnpause.addEventListener("click", () => {
+    t === 0 ? (t = 1) : (t = 0);
+    if (t === 1) {
+      clearTimeout(timer);
+    } else {
+      setTimeout(counter);
+    }
+  });
+
+  counter();
 });
-
-btnplus.addEventListener('click', () => {
-    count.textContent = (iter++);
-    //count.value = parseInt(count.value) + 1;
-});
-
-btnheart.addEventListener('click', () => {
-    let val = count.textContent
-    likeUl.appendChild(likeNum)
-    likeNum.textContent = val
-})
-
-
- btnpause.addEventListener('click', () =>  {
-    
-    clearTimeout(counter)
-})
-
-
-
-counter();
-
-})
